b18b60eaba7a06b39aa2be67773eb64fb3170fa9
Andre Cavalcanti Arruda Pazzetti <aapazzetti@energia.org.br>
2020-04-03 14:27:08 -0300

    Refactor of Masthead component and Person service

--- poc-frontend/src/app/components/masthead/masthead.component.ts
+++ poc-frontend/src/app/components/masthead/masthead.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,16 +8,16 @@ import { Router } from '@angular/router';
 })
 export class MastheadComponent implements OnInit {
 
-  title: string;
-  subtitle: string;
+  @Input() title: string;
+  @Input() subtitle: string;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
-    if (this.router.url === '/home') {
-      this.title = "Client Register POC"
-      this.subtitle = "An Angular Project"
-    }
+    // if (this.router.url === '/home') {
+    //   this.title = "Client Register POC"
+    //   this.subtitle = "An Angular Project"
+    // }
   }
 
 }
