922e82b7310da2d3f344bc320dfee8ccd3767da6
Andre Cavalcanti Arruda Pazzetti <aapazzetti@energia.org.br>
2020-05-08 18:09:13 -0300

    08/05 Changes

--- poc-frontend/src/app/components/home/home.component.ts
+++ poc-frontend/src/app/components/home/home.component.ts
@@ -75,7 +75,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
        this.lowValue = this.lowValue + this.pageSize;
        this.highValue =  this.highValue + this.pageSize;
       }
-   else if(event.pageIndex === this.pageIndex - 1){
+    else if(event.pageIndex === this.pageIndex - 1){
       this.lowValue = this.lowValue - this.pageSize;
       this.highValue =  this.highValue - this.pageSize;
      }   
