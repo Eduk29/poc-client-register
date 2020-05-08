e4dd2abd154e0e53595a4ffdba5141657d56de83
Andre Cavalcanti Arruda Pazzetti <aapazzetti@energia.org.br>
2020-05-08 18:05:31 -0300

    commit 08/05

--- poc-frontend/src/app/components/home/home.component.ts
+++ poc-frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+
 import { PersonService } from "src/app/services/persons/person.service";
 
 import { Contact } from "src/app/models/contact.model";
 import { Document } from "src/app/models/document.model";
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements OnInit {
+
+
+export class HomeComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   displayedColumns: string[] = [
     "position",
     "name",
@@ -17,9 +25,18 @@ export class HomeComponent implements OnInit {
     "contact",
     "document"
   ];
-  //dataSource = new PersonDataSource(this.personService);
   dataSource = this.dataSource;
   persons: any;
+  length = 1000;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 15];
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+  pageIndex:number = 0;
+  lowValue:number = 0;
+  highValue:number = 10;   
+  
 
   constructor(private personService: PersonService) {}
 
@@ -32,11 +49,18 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  ngAfterViewInit() {
+    this.paginator.page.subscribe(
+       (event) => console.log(event)
+    );
+  }
+
   getNewMainContact(contacts: Array<Contact>): string {
     const newContacts = contacts.filter(contact => contact.isPrincipal === true);
     return newContacts[0].value;
   }
 
+  
 
   getNewRGDocument(documents: Array<Document>): string {
     const newDocuments = documents
@@ -44,7 +68,22 @@ export class HomeComponent implements OnInit {
       .shift();
     return !!newDocuments ? newDocuments.documentValue : '-';
   }
+
+  getPaginatorData(event){
+    console.log(event);
+    if(event.pageIndex === this.pageIndex + 1){
+       this.lowValue = this.lowValue + this.pageSize;
+       this.highValue =  this.highValue + this.pageSize;
+      }
+   else if(event.pageIndex === this.pageIndex - 1){
+      this.lowValue = this.lowValue - this.pageSize;
+      this.highValue =  this.highValue - this.pageSize;
+     }   
+      this.pageIndex = event.pageIndex;
 }
+}
+
+
    
    
 
