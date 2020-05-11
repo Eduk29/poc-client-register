b6731a296b1f3ef2913ad94aa7d0a9f1e74a8a29
Andre Cavalcanti Arruda Pazzetti <aapazzetti@energia.org.br>
2020-05-11 17:59:33 -0300

    functioning paginator

--- poc-frontend/src/app/components/home/home.component.ts
+++ poc-frontend/src/app/components/home/home.component.ts
@@ -4,18 +4,16 @@ import { PersonService } from "src/app/services/persons/person.service";
 
 import { Contact } from "src/app/models/contact.model";
 import { Document } from "src/app/models/document.model";
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { Person } from "src/app/models/person.model";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-
-
 export class HomeComponent implements OnInit, AfterViewInit {
-  
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   displayedColumns: string[] = [
@@ -25,66 +23,57 @@ export class HomeComponent implements OnInit, AfterViewInit {
     "contact",
     "document"
   ];
-  dataSource = this.dataSource;
+  dataSource: MatTableDataSource<Person>;
   persons: any;
-  length = 1000;
+  length: number;
   pageSize = 10;
+  pageIndex = 1;
   pageSizeOptions: number[] = [5, 10, 15];
-  // MatPaginator Output
-  pageEvent: PageEvent;
 
-  pageIndex:number = 0;
-  lowValue:number = 0;
-  highValue:number = 10;   
-  
-
-  constructor(private personService: PersonService) {}
+  constructor(private personService: PersonService) {
+    this.dataSource = new MatTableDataSource(this.persons);
+  }
 
   ngOnInit() {
-    this.persons = this.personService.getPersons();
-
-    this.persons.subscribe((data) => {
-      this.dataSource = data;
-      console.log(data);
-    });
+    this.dataSource.paginator = this.paginator;
   }
 
   ngAfterViewInit() {
-    this.paginator.page.subscribe(
-       (event) => console.log(event)
-    );
+    this.getDataLength();
+  }
+
+  getDataPaginated(): void {
+    this.personService
+      .getPersons(this.pageSize, this.pageIndex)
+      .subscribe((data: any) => {
+        this.dataSource = data;
+      });
+  }
+
+  getDataLength(): void {
+    this.personService.getPersonsRaw().subscribe((data: Array<Person>) => {
+      this.length = data.length;
+      this.getDataPaginated();
+    });
+  }
+
+  getPage(event) {
+    this.pageIndex = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.getDataPaginated();
   }
 
   getNewMainContact(contacts: Array<Contact>): string {
-    const newContacts = contacts.filter(contact => contact.isPrincipal === true);
+    const newContacts = contacts.filter(
+      contact => contact.isPrincipal === true
+    );
     return newContacts[0].value;
   }
 
-  
-
   getNewRGDocument(documents: Array<Document>): string {
     const newDocuments = documents
-      .filter((document: Document) => document.documentType.key === 'RG')
+      .filter((document: Document) => document.documentType.key === "RG")
       .shift();
-    return !!newDocuments ? newDocuments.documentValue : '-';
+    return !!newDocuments ? newDocuments.documentValue : "-";
   }
-
-  getPaginatorData(event){
-    console.log(event);
-    if(event.pageIndex === this.pageIndex + 1){
-       this.lowValue = this.lowValue + this.pageSize;
-       this.highValue =  this.highValue + this.pageSize;
-      }
-    else if(event.pageIndex === this.pageIndex - 1){
-      this.lowValue = this.lowValue - this.pageSize;
-      this.highValue =  this.highValue - this.pageSize;
-     }   
-      this.pageIndex = event.pageIndex;
 }
-}
-
-
-   
-   
-
-
